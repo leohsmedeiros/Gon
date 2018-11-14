4ee6c6e487b97b40b5acf615aaefed6b85546631
Leonardo Medeiros <leohsmedeiros@gmail.com>
2018-11-14 14:28:49 -0200

    first commit

---
+++ Assets/Scripts/SliderBehavior.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;// Required when using Event data.
+
+public class SliderBehavior : MonoBehaviour,
+IEventSystemHandler,
+IPointerUpHandler,
+IPointerDownHandler {
+
+	public Slider slider;
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		if (eventData.button != PointerEventData.InputButton.Left)
+			return;
+		
+		// Selection tracking
+		EventSystem.current.SetSelectedGameObject(gameObject, eventData);
+	}
+
+	public void OnPointerUp (PointerEventData data) 
+	{
+//		data.
+		slider.value = 0;
+//		Debug.Log ("Deselected");
+	}
+	
+}
