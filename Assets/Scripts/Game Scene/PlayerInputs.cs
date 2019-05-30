fbb7f64c9bb8b21df8ce09770233291db0954c23
Leonardo Medeiros <>
2019-05-29 23:50:37 -0300

    - imported and integrated a mobile joystick asset

--- Assets/Scripts/Game Scene/PlayerInputs.cs
+++ Assets/Scripts/Game Scene/PlayerInputs.cs
@@ -5,8 +5,10 @@ using UniRx.Triggers;
 
 namespace Gon {
     public class PlayerInputs : MonoBehaviour  {
-        public RxOnMouseOver btnMoveBack;
-        public RxOnMouseOver btnMoveForward;
+        //public RxOnMouseOver btnMoveBack;
+        //public RxOnMouseOver btnMoveForward;
+
+        public FixedJoystick joystick;
         public Button btnJump;
         public Button btnAttack;
 
@@ -14,17 +16,53 @@ namespace Gon {
         public Rigidbody2D playerRigidbody;
 
         private void Start() {
-            btnMoveBack.onMouseOver.Subscribe(_ => {
-                playerAnimator.SetBool("move", true);
-                LeanTween.scaleX(playerAnimator.gameObject, -1, Time.deltaTime);
-                LeanTween.moveX(playerAnimator.gameObject, playerAnimator.transform.position.x - 0.5f, Time.deltaTime);
+            this.gameObject.UpdateAsObservable().Subscribe(_ => {
+                Debug.Log("joystick.Horizontal: " + joystick.Horizontal);
+
+                if (joystick.Horizontal < 0) {
+                    playerAnimator.SetBool("move", true);
+                    LeanTween.scaleX(playerAnimator.gameObject, -1, Time.deltaTime);
+                } else if (joystick.Horizontal > 0) {
+                    playerAnimator.SetBool("move", true);
+                    LeanTween.scaleX(playerAnimator.gameObject, 1, Time.deltaTime);
+                }else {
+                    playerAnimator.SetBool("move", false);
+                }
+
+                LeanTween.moveX(playerAnimator.gameObject, playerAnimator.transform.position.x + joystick.Horizontal, Time.deltaTime);
+
+                //if (joystick.Horizontal < 0) {
+                //    playerAnimator.SetBool("move", true);
+                //    LeanTween.scaleX(playerAnimator.gameObject, -1, Time.deltaTime);
+                //    LeanTween.moveX(playerAnimator.gameObject, playerAnimator.transform.position.x + -1, Time.deltaTime);
+                //} else if (joystick.Horizontal > 0) {
+                //    playerAnimator.SetBool("move", true);
+                //    LeanTween.scaleX(playerAnimator.gameObject, 1, Time.deltaTime);
+                //    LeanTween.moveX(playerAnimator.gameObject, playerAnimator.transform.position.x + 1, Time.deltaTime);
+                //} else {
+                //    playerAnimator.SetBool("move", false);
+                //}
+
+
+                //if (joystick.Horizontal < 0) move = -1;
+                //else if (joystick.Horizontal > 0) move = 1;
+                //else move = 0;
+
+                //LeanTween.scaleX(playerAnimator.gameObject, move, Time.deltaTime);
+                //LeanTween.moveX(playerAnimator.gameObject, playerAnimator.transform.position.x + move, Time.deltaTime);
             });
 
-            btnMoveForward.onMouseOver.Subscribe(_ => {
-                playerAnimator.SetBool("move", true);
-                LeanTween.scaleX(playerAnimator.gameObject, 1, Time.deltaTime);
-                LeanTween.moveX(playerAnimator.gameObject, playerAnimator.transform.position.x + 0.5f, Time.deltaTime);
-            });
+            //btnMoveBack.onMouseOver.Subscribe(_ => {
+            //    playerAnimator.SetBool("move", true);
+            //    LeanTween.scaleX(playerAnimator.gameObject, -1, Time.deltaTime);
+            //    LeanTween.moveX(playerAnimator.gameObject, playerAnimator.transform.position.x - 0.5f, Time.deltaTime);
+            //});
+
+            //btnMoveForward.onMouseOver.Subscribe(_ => {
+            //    playerAnimator.SetBool("move", true);
+            //    LeanTween.scaleX(playerAnimator.gameObject, 1, Time.deltaTime);
+            //    LeanTween.moveX(playerAnimator.gameObject, playerAnimator.transform.position.x + 0.5f, Time.deltaTime);
+            //});
 
             btnJump.OnPointerDownAsObservable().ThrottleFirst(new System.TimeSpan(0, 0, 2)).Subscribe(_ => {
                 playerRigidbody.AddForce(Vector3.up * 2500);
